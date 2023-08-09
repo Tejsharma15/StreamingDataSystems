e3edd6d9dc8b0bd5a619c98ec4944ed7eb3ad56f
Tejsharma15 <tejsharma815@gmail.com>
2023-08-09 10:54:05 +0530

    Modified query 1 from 10 milliseconds to 10 seconds

--- Assignment1/queries.sql
+++ Assignment1/queries.sql
@@ -1,10 +1,10 @@
-SELECT m.c_id as campaignId, floor(d.event_time/10) as eventTime, count(*) as count 
+SELECT m.c_id as campaignId, floor(d.event_time/10000) as eventTime, count(*) as count 
 FROM  data d INNER JOIN mappings m ON 
 m.a_id = d.a_id WHERE d.event_type = 'click' 
-GROUP BY m.c_id, floor(d.event_time/10) 
+GROUP BY m.c_id, floor(d.event_time/10000) 
 ORDER BY m.c_id, eventTime asc;
 
-
-SELECT count(*)/14.073 AS throughtput
-FROM data d;
+-- USING EXPLAIN ANALYZE - Find the execution time
+-- SELECT count(*)/14.073 AS throughtput
+-- FROM data d;
 
